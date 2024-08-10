79dec7d0bd65e5455c9884ea5524eb9613c60e4d
OMD site ServerMonitoring <ServerMonitoring@checkmk.local>
2024-08-10 15:21:59 +0200

    fix for cmk 2.3

--- lib/check_mk/base/plugins/agent_based/unifi_controller.py
+++ lib/check_mk/base/plugins/agent_based/unifi_controller.py
@@ -2,19 +2,19 @@
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 #
 ##  MIT License
-##  
+##
 ##  Copyright (c) 2021 Bash Club
-##  
+##
 ##  Permission is hereby granted, free of charge, to any person obtaining a copy
 ##  of this software and associated documentation files (the "Software"), to deal
 ##  in the Software without restriction, including without limitation the rights
 ##  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 ##  copies of the Software, and to permit persons to whom the Software is
 ##  furnished to do so, subject to the following conditions:
-##  
+##
 ##  The above copyright notice and this permission notice shall be included in all
 ##  copies or substantial portions of the Software.
-##  
+##
 ##  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ##  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ##  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -270,7 +270,6 @@ register.inventory_plugin(
 def discovery_unifi_device(section):
     yield Service(item="Device Status")
     yield Service(item="Unifi Device")
-    yield Service(item="Unifi Device Uptime")
     yield Service(item="Active-User")
     if  section.type != "uap":  # kein satisfaction bei ap .. radio/ssid haben schon
         yield Service(item="Satisfaction")
@@ -316,14 +315,7 @@ def check_unifi_device(item,section):
             )
         yield Metric("user_sta",_active_user)
         yield Metric("guest_sta",_safe_int(section.guest_num_sta))
-    if item == "Unifi Device Uptime":
-        _uptime = int(section.uptime) if section.uptime else -1
-        if _uptime > 0:
-            yield Result(
-                state=State.OK,
-                summary=render.timespan(_uptime)
-            )
-            yield Metric("unifi_uptime",_uptime)
+
     if item == "Satisfaction":
         yield Result(
             state=State.OK,
@@ -362,7 +354,7 @@ def check_unifi_device(item,section):
         yield Metric("rtt",_safe_float(section.speedtest_ping))
         yield Metric("if_in_bps",_safe_float(section.speedtest_download)*1024*1024) ## mbit to bit
         yield Metric("if_out_bps",_safe_float(section.speedtest_upload)*1024*1024) ## mbit to bit
-            
+
     if item == "Uplink":
         yield Result(
             state=_expect_bool(section.uplink_up),
@@ -415,7 +407,7 @@ register.inventory_plugin(
 
 ############ DEVICEPORT ###########
 @dataclass
-class unifi_interface(interfaces.Interface):
+class unifi_interface(interfaces.InterfaceWithCounters):
     jumbo           : bool = False
     satisfaction    : int = 0
     poe_enable      : bool = False
@@ -429,9 +421,6 @@ class unifi_interface(interfaces.Interface):
     dot1x_status    : Optional[str] = None
     ip_address      : Optional[str] = None
     portconf        : Optional[str] = None
-
-    def __post_init__(self) -> None:
-        self.finalize()
 
 def _convert_unifi_counters_if(section: Section) -> interfaces.Section:
     ##  10|port_idx|10
@@ -486,27 +475,31 @@ def _convert_unifi_counters_if(section: Section) -> interfaces.Section:
     ##      poe_enable=False, poe_mode='auto', poe_good=None, poe_current=0.0, poe_power=0.0, poe_voltage=0.0, poe_class='Unknown', 
     ##      dot1x_mode='unknown',dot1x_status='disabled', ip_address='', portconf='ALL')
 
-    return [ 
+    return [
         unifi_interface(
-            index=str(netif.port_idx),
-            descr=netif.name,
-            alias=netif.name,
-            type='6',
-            speed=_safe_int(netif.speed)*1000000,
-            oper_status=netif.oper_status,
-            admin_status=netif.admin_status,
-            in_octets=_safe_int(netif.rx_bytes),
-            in_ucast=_safe_int(netif.rx_packets),
-            in_mcast=_safe_int(netif.rx_multicast),
-            in_bcast=_safe_int(netif.rx_broadcast),
-            in_discards=_safe_int(netif.rx_dropped),
-            in_errors=_safe_int(netif.rx_errors),
-            out_octets=_safe_int(netif.tx_bytes),
-            out_ucast=_safe_int(netif.tx_packets),
-            out_mcast=_safe_int(netif.tx_multicast),
-            out_bcast=_safe_int(netif.tx_broadcast),
-            out_discards=_safe_int(netif.tx_dropped),
-            out_errors=_safe_int(netif.tx_errors),
+            attributes=interfaces.Attributes(
+                index=str(netif.port_idx),
+                descr=netif.name,
+                alias=netif.name,
+                type='6',
+                speed=_safe_int(netif.speed)*1000000,
+                oper_status=netif.oper_status,
+                admin_status=netif.admin_status,
+            ),
+            counters=interfaces.Counters(
+                in_octets=_safe_int(netif.rx_bytes),
+                in_ucast=_safe_int(netif.rx_packets),
+                in_mcast=_safe_int(netif.rx_multicast),
+                in_bcast=_safe_int(netif.rx_broadcast),
+                in_disc=_safe_int(netif.rx_dropped),
+                in_err=_safe_int(netif.rx_errors),
+                out_octets=_safe_int(netif.tx_bytes),
+                out_ucast=_safe_int(netif.tx_packets),
+                out_mcast=_safe_int(netif.tx_multicast),
+                out_bcast=_safe_int(netif.tx_broadcast),
+                out_disc=_safe_int(netif.tx_dropped),
+                out_err=_safe_int(netif.tx_errors),
+            ),
             jumbo=True if netif.jumbo == "1" else False,
             satisfaction=_safe_int(netif.satisfaction) if netif.satisfaction and netif.oper_status == "1" else 0,
             poe_enable=True if netif.poe_enable == "1" else False,
@@ -521,7 +514,7 @@ def _convert_unifi_counters_if(section: Section) -> interfaces.Section:
             portconf=netif.portconf
         ) for netif in parse_unifi_nested_dict(section).values()
     ]
-    
+
 
 
 def discovery_unifi_network_port_if(  ## fixme parsed_section_name
@@ -540,7 +533,7 @@ def check_unifi_network_port_if(  ##fixme parsed_section_name
     section: Section,
 ) -> CheckResult:
     _converted_ifs = _convert_unifi_counters_if(section)
-    iface = next(filter(lambda x: _safe_int(item,-1) == _safe_int(x.index) or item == x.alias,_converted_ifs),None) ## fix Service Discovery appearance alias/descr
+    iface = next(filter(lambda x: _safe_int(item,-1) == _safe_int(x.attributes.index) or item == x.attributes.alias,_converted_ifs),None) ## fix Service Discovery appearance alias/descr
     yield from interfaces.check_multiple_interfaces(
         item,
         params,
@@ -607,7 +600,7 @@ register.check_plugin(
     discovery_ruleset_type=register.RuleSetType.ALL,
     discovery_default_parameters=dict(interfaces.DISCOVERY_DEFAULT_PARAMETERS),
     discovery_function=discovery_unifi_network_port_if,
-    check_ruleset_name="if",
+    check_ruleset_name="interfaces",
     check_default_parameters=interfaces.CHECK_DEFAULT_PARAMETERS,
     check_function=check_unifi_network_port_if,
 )
